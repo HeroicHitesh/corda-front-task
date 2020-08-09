67a1debe28d83214183a186868d95e8452e2c91a
HeroicHitesh <hiteshkumar_1mv17cs042@sirmvit.edu>
2020-08-09 22:16:56 +0530

    Updates

---
+++ precache-manifest.846396d4259786aaa9ad456394f79115.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8851a5355aaeac49c5bb57e91fcac822",
+    "url": "/'https://heroichitesh.github.io/corda-front-task/'/index.html"
+  },
+  {
+    "revision": "6f63727dc4bb75f80ebe",
+    "url": "/'https://heroichitesh.github.io/corda-front-task/'/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "d487d1db4d17209e6c9c",
+    "url": "/'https://heroichitesh.github.io/corda-front-task/'/static/css/main.6a0be5f0.chunk.css"
+  },
+  {
+    "revision": "6f63727dc4bb75f80ebe",
+    "url": "/'https://heroichitesh.github.io/corda-front-task/'/static/js/2.10303f08.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/'https://heroichitesh.github.io/corda-front-task/'/static/js/2.10303f08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d487d1db4d17209e6c9c",
+    "url": "/'https://heroichitesh.github.io/corda-front-task/'/static/js/main.13e3f898.chunk.js"
+  },
+  {
+    "revision": "69015e32a4bd3f62ad8a",
+    "url": "/'https://heroichitesh.github.io/corda-front-task/'/static/js/runtime-main.a1aca95b.js"
+  }
+]);
